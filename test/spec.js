f19223d526ec61c6c2d98454d4e102dc9c59999a
Omar Kamali <omar.kamali@gmail.com>
2014-05-21 14:39:25 +0100

    changed spec for cartesian

--- test/spec.js
+++ test/spec.js
@@ -1,15 +1,17 @@
 var assert = require("assert")
+var should = require("should")
 var cartesian = require("../lib/cartesian")
 
 describe('Cartesian', function() {
     it('should be loaded correctly', function() {
-        should(cartesian).exist;
+        console.log(cartesian)
+        should(cartesian).not.equal(undefined);
     });
     it('should store the cartesian coordinates of a point', function() {
         var x = 10;
         var y = 5;
         var point = cartesian.init(x, y);
-        point.should.have.property('x', x)
-        point.should.have.property('y', y)
-    })
+        point.x().should.equal(x)
+        point.y().should.equal(y)
+    });
 })
