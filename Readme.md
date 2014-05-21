1a2265e5af390fdbc58e5916999c68b18a7d849a
Omar Kamali <omar.kamali@gmail.com>
2014-05-21 16:04:54 +0100

    tests & implementation of setPosition for cartesian

--- Readme.md
+++ Readme.md
@@ -1,8 +1,16 @@
 PlanarJS
 =====
 
-A lightweight JS library for planar ( 2D ) coordinates management.
+A lightweight JS library to work with planar ( 2D ) coordinates.
 Supports the following coordinate systems :
-* Cartesian ( soon )
+* Cartesian ( partial )
 * Polar ( soon )
 * Geographic ( soon )
+
+TODO :
+=
+
+* Provide events for listening to changes in coordinates
+* Wrap all coordinates systems in one object
+* Implement conversion functions between coordinate systems
+* Implement facilities for computing distances between points
--- test/spec.js
+++ test/spec.js
@@ -1,17 +1,29 @@
 var assert = require("assert")
 var should = require("should")
 var cartesian = require("../lib/cartesian")
+var polar = require("../lib/polar")
 
 describe('Cartesian', function() {
     it('should be loaded correctly', function() {
-        console.log(cartesian)
         should(cartesian).not.equal(undefined);
     });
     it('should store the cartesian coordinates of a point', function() {
         var x = 10;
         var y = 5;
         var point = cartesian.init(x, y);
-        point.x().should.equal(x)
-        point.y().should.equal(y)
+        point.x().should.equal(x);
+        point.y().should.equal(y);
     });
-})
+    it('should change the value of the coordinates of a point', function() {
+        var x1 = 10;
+        var y1 = 5;
+        var x2 = 2;
+        var y2 = 8;
+        var point = cartesian.init(x1, y1);
+        point.x().should.equal(x1);
+        point.y().should.equal(y1);
+        point.setPosition(x2, y2);
+        point.x().should.equal(x2);
+        point.y().should.equal(y2);
+    });
+});
