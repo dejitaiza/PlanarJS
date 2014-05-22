ff1417b42f996e1d0c0456084e1ea03b40b25566
Omar Kamali <omar.kamali@gmail.com>
2014-05-22 17:32:24 +0100

    Tests & implementation for polar to cartesian conversion

--- test/spec.js
+++ test/spec.js
@@ -119,4 +119,52 @@ describe('Utilities', function() {
         polarPoint9.th().should.equal(Math.PI * (1 / 4));
 
     });
+    it('should convert polar coordinates to cartesian', function() {
+        var delta = 0.000000000000001;
+        var polarPoint1 = polar.init(0, 0);
+        console.log(polarPoint1);
+        var cartPoint1 = utilities.polarToCartesian(polarPoint1);
+        cartPoint1.x().should.be.within(0 - delta, 0 + delta);
+        cartPoint1.y().should.be.within(0 - delta, 0 + delta);
+
+        var polarPoint2 = polar.init(1, 0);
+        var cartPoint2 = utilities.polarToCartesian(polarPoint2);
+        cartPoint2.x().should.be.within(1 - delta, 1 + delta);
+        cartPoint2.y().should.be.within(0 - delta, 0 + delta);
+
+        var polarPoint3 = polar.init(1, Math.PI / 2);
+        var cartPoint3 = utilities.polarToCartesian(polarPoint3);
+        cartPoint3.x().should.be.within(0 - delta, 0 + delta);
+        cartPoint3.y().should.be.within(1 - delta, 1 + delta);
+
+        var polarPoint4 = polar.init(Math.sqrt(2), Math.PI * (-3 / 4));
+        var cartPoint4 = utilities.polarToCartesian(polarPoint4);
+        cartPoint4.x().should.be.within(-1 - delta, -1 + delta);
+        cartPoint4.y().should.be.within(-1 - delta, -1 + delta);
+
+        var polarPoint5 = polar.init(1, Math.PI);
+        var cartPoint5 = utilities.polarToCartesian(polarPoint5);
+        cartPoint5.x().should.be.within(-1 - delta, -1 + delta);
+        cartPoint5.y().should.be.within(0 - delta, 0 + delta);
+
+        var polarPoint6 = polar.init(Math.sqrt(2), Math.PI * (3 / 4));
+        var cartPoint6 = utilities.polarToCartesian(polarPoint6);
+        cartPoint6.x().should.be.within(-1 - delta, -1 + delta);
+        cartPoint6.y().should.be.within(1 - delta, 1 + delta);
+
+        var polarPoint7 = polar.init(1, Math.PI * (-1 / 2));
+        var cartPoint7 = utilities.polarToCartesian(polarPoint7);
+        cartPoint7.x().should.be.within(0 - delta, 0 + delta);
+        cartPoint7.y().should.be.within(-1 - delta, -1 + delta);
+
+        var polarPoint8 = polar.init(Math.sqrt(2), Math.PI * (-1 / 4));
+        var cartPoint8 = utilities.polarToCartesian(polarPoint8);
+        cartPoint8.x().should.be.within(1 - delta, 1 + delta);
+        cartPoint8.y().should.be.within(-1 - delta, -1 + delta);
+
+        var polarPoint9 = polar.init(Math.sqrt(2), Math.PI * (1 / 4));
+        var cartPoint9 = utilities.polarToCartesian(polarPoint9);
+        cartPoint9.x().should.be.within(1 - delta, 1 + delta);
+        cartPoint9.y().should.be.within(1 - delta, 1 + delta);
+    });
 });
