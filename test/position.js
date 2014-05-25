48008057e2d4dd9fe93dae3d271364d3301c59d9
Omar Kamali <omar.kamali@gmail.com>
2014-05-25 12:12:27 +0100

    test & implementation support for vector ( basic linear algebra )

--- test/position.js
+++ test/position.js
@@ -63,6 +63,21 @@ describe('Position', function() {
         pos.r().should.be.within(1 - delta, 1 + delta);
         pos.th().should.be.within((Math.PI / 2) - delta, (Math.PI / 2) + delta);
     });
+    it('should update the coordinates using the generalized set function', function() {
+        var pos = position.init('cartesian', 3, 3);
+
+        pos.set('polar', 1, 0);
+        pos.r().should.equal(1);
+        pos.th().should.equal(0);
+        pos.x().should.be.within(1 - delta, 1 + delta);
+        pos.y().should.be.within(0 - delta, 0 + delta);
+
+        pos.set('cartesian', 0, 1);
+        pos.x().should.equal(0);
+        pos.y().should.equal(1);
+        pos.r().should.be.within(1 - delta, 1 + delta);
+        pos.th().should.be.within((Math.PI / 2) - delta, (Math.PI / 2) + delta);
+    });
     it('should compute the distance between two points', function() {
         var posA = position.init('cartesian', 0, 0);
         var posB = position.init('cartesian', 1, 1);
---
+++ test/vector.js
@@ -0,0 +1,52 @@
+var should = require("should")
+var position = require("../lib/position")
+var vector = require("../lib/vector")
+
+describe('Vector', function() {
+    it('should be loaded correctly', function() {
+        should(vector).not.equal(undefined);
+    });
+    it('should return the coordinates used to construct it', function() {
+        var a = vector.init('cartesian', 2, 2);
+        a.x().should.equal(2);
+        a.y().should.equal(2);
+    });
+    it('should add vectors and return the result', function() {
+        var a = vector.init('cartesian', 2, 3);
+        var b = vector.init('cartesian', 4, 5);
+        var c = vector.add(a, b);
+        c.x().should.equal(6);
+        c.y().should.equal(8);
+    });
+    it('should subtract vectors and return the result', function() {
+        var a = vector.init('cartesian', 2, 3);
+        var b = vector.init('cartesian', 4, 5);
+        var c = vector.subtract(a, b);
+        c.x().should.equal(-2);
+        c.y().should.equal(-2);
+    });
+    it('should initialize a vector from two points', function() {
+        var pointA = position.init('cartesian', 0, 2);
+        var pointB = position.init('cartesian', 5, 2);
+        var vectorAB = vector.fromPoints(pointA, pointB);
+        vectorAB.x().should.equal(5);
+        vectorAB.y().should.equal(0);
+    });
+    it('should obey Chasles relation ', function() {
+        var pointO = position.init('cartesian', 0, 0);
+        var pointA = position.init('cartesian', 4, 2);
+        var pointB = position.init('cartesian', 8, 3);
+        var vectorAO = vector.fromPoints(pointA, pointO);
+        var vectorOB = vector.fromPoints(pointO, pointB);
+        var vectorAB = vector.fromPoints(pointA, pointB);
+        var sumAOOB = vector.add(vectorAO, vectorOB);
+        vectorAB.x().should.equal(sumAOOB.x());
+        vectorAB.y().should.equal(sumAOOB.y());
+    });
+    it('should compute a dot product between two vectors', function() {
+        var a = vector.init('cartesian', 2, 3);
+        var b = vector.init('cartesian', 4, 5);
+        var product = vector.dot(a, b);
+        product.should.equal(23);
+    });
+});
